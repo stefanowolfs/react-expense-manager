7368bde0ded0c0760ed3f1df540bea82c5667e6e
Stefano Soro <stefano.soro@concur.com>
2022-07-04 16:23:41 -0300

    💄 Add clean global styles

--- src/styles/globalStyles.ts
+++ src/styles/globalStyles.ts
@@ -1,19 +1,157 @@
 import { createGlobalStyle } from "styled-components";
 
 const GlobalStyle = createGlobalStyle`
-  body {
-    font-family: Open-Sans, Helvetica, Sans-Serif;
-    margin: 0;
-    padding: 0;
+   /* http://meyerweb.com/eric/tools/css/reset/
+   v2.0 | 20110126
+   License: none (public domain)
+*/
 
-    background: rgb(12, 93, 196);
-    background: linear-gradient(
-      90deg,
-      ${(props) => props.theme.palette.gradientBackground.color1} 0%,
-      ${(props) => props.theme.palette.gradientBackground.color2} 100%
-    );
+html,
+  body,
+  div,
+  span,
+  applet,
+  object,
+  iframe,
+  h1,
+  h2,
+  h3,
+  h4,
+  h5,
+  h6,
+  p,
+  blockquote,
+  pre,
+  a,
+  abbr,
+  acronym,
+  address,
+  big,
+  cite,
+  code,
+  del,
+  dfn,
+  em,
+  img,
+  ins,
+  kbd,
+  q,
+  s,
+  samp,
+  small,
+  strike,
+  strong,
+  sub,
+  sup,
+  tt,
+  var,
+  b,
+  u,
+  i,
+  center,
+  dl,
+  dt,
+  dd,
+  ol,
+  ul,
+  li,
+  fieldset,
+  form,
+  label,
+  legend,
+  table,
+  caption,
+  tbody,
+  tfoot,
+  thead,
+  tr,
+  th,
+  td,
+  article,
+  aside,
+  canvas,
+  details,
+  embed,
+  figure,
+  figcaption,
+  footer,
+  header,
+  hgroup,
+  menu,
+  nav,
+  output,
+  ruby,
+  section,
+  summary,
+  time,
+  mark,
+  audio,
+  video {
     margin: 0;
     padding: 0;
+    border: 0;
+    font-size: 100%;
+    font: inherit;
+    vertical-align: baseline;
+  }
+  /* HTML5 display-role reset for older browsers */
+  article,
+  aside,
+  details,
+  figcaption,
+  figure,
+  footer,
+  header,
+  hgroup,
+  menu,
+  nav,
+  section {
+    display: block;
+  }
+  body {
+    line-height: 1;
+  }
+  ol,
+  ul {
+    list-style: none;
+  }
+  blockquote,
+  q {
+    quotes: none;
+  }
+  blockquote:before,
+  blockquote:after,
+  q:before,
+  q:after {
+    content: '';
+    content: none;
+  }
+  table {
+    border-collapse: collapse;
+    border-spacing: 0;
+  }
+
+  html,
+  body {
+    height: 100%;
+  }
+
+  #root {
+    height: 100%;
+  }
+
+  body {
+    margin: 0;
+    text-decoration: optimizeLegibility !important;
+    -webkit-font-smothing: antialiased !important;
+    background: #fff;
+    font-family: 'Source Sans Pro', sans-serif;
+    color: #181818;
+  }
+
+  .btn__transparent {
+    background: transparent;
+    border: none;
   }
 `;
 
