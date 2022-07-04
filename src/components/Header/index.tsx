27c35f756896ccac9585d27094949e102c3cdf9a
Stefano Soro <stefano.soro@concur.com>
2022-07-04 17:50:08 -0300

    ✏️  Add identifier to selected navLink

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,20 +1,33 @@
-import { AppBar, CssBaseline, Toolbar, Typography } from "@mui/material";
-import React from "react";
+import { AppBar, CssBaseline, Toolbar } from "@mui/material";
+import React, { useEffect } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
 import { logout } from "../../core/store/ducks/app";
 import { useAppDispatch } from "../../core/store/hooks";
-import { Link, LinkContainer } from "./styles";
+import { Link, LinkContainer, Title } from "./styles";
 
 const Header: React.FC = () => {
   const dispatch = useAppDispatch();
+  const location = useLocation();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    console.log(location.pathname);
+  }, [location]);
 
   return (
     <AppBar position="static">
       <CssBaseline />
       <Toolbar>
-        <Typography variant="h4">Expense Manager</Typography>
+        <Title variant="h4" onClick={() => navigate("/home")}>
+          Expense Manager
+        </Title>
         <LinkContainer>
-          <Link to="/home">Home</Link>
-          <Link to="/expenses">Expenses</Link>
+          <Link to="/home" active={location.pathname.includes("home")}>
+            Home
+          </Link>
+          <Link to="/expenses" active={location.pathname.includes("expenses")}>
+            Expenses
+          </Link>
         </LinkContainer>
         <button type="button" onClick={() => dispatch(logout())}>
           Sign Out
--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -1,5 +1,11 @@
 import styled from "styled-components";
-import { NavLink } from "react-router-dom";
+import { NavLink, NavLinkProps } from "react-router-dom";
+import { Typography } from "@mui/material";
+import Colors from "../../styles/colors";
+
+interface LinkProps {
+  active?: boolean;
+}
 
 export const LinkContainer = styled.div`
   flex: 1;
@@ -10,8 +16,25 @@ export const LinkContainer = styled.div`
   margin-right: 40px;
 `;
 
-export const Link = styled(NavLink)`
+export const Link = styled<
+  React.ForwardRefExoticComponent<
+    LinkProps & NavLinkProps & React.RefAttributes<HTMLAnchorElement>
+  >
+>(NavLink)`
   margin: 15px;
   margin-top: 0px;
   margin-bottom: 0px;
+  text-decoration: none;
+  color: ${({ active }) => (active === true ? Colors.black : Colors.white)};
+  font-size: 20px;
+  margin-left: 20px;
+  hover: {
+    color: yellow;
+    border-bottom: 1px solid white;
+  }
+`;
+
+export const Title = styled(Typography)`
+  flex-grow: 1;
+  cursor: pointer;
 `;
