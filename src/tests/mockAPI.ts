eb6ba37d9f29e0bf6ffe455ad70097e868ddac87
Stefano Soro <stefano.soro@concur.com>
2022-07-06 23:34:06 -0300

    🤡  Add some mocks

--- src/tests/mockAPI.ts
+++ src/tests/mockAPI.ts
@@ -17,6 +17,66 @@ export function startMockAPI() {
       this.get("/login", () => {
         return { token: "2389d8998-83298ds8fd98-dsf8787f7s" };
       });
+
+      this.get("/expenses/report", () => {
+        return {
+          totalCost: 2816,
+          reportRange: {
+            from: new Date(),
+            to: new Date(),
+          },
+          userId: 1,
+          costCenters: [
+            {
+              name: "Mobilidade",
+              id: 1,
+              expenseGroups: [
+                { name: "Carro", cost: 2050.6, id: 4245 },
+                { name: "Transporte", cost: 520.6, id: 6685 },
+              ],
+            },
+            {
+              name: "Saúde",
+              id: 2,
+              expenseGroups: [
+                { name: "Remedio", cost: 44.8, id: 2235 },
+                { name: "Consulta", cost: 200, id: 6548 },
+              ],
+            },
+          ],
+        };
+      });
+
+      this.get("/expense-groups/:id/activities", (schema, request) => {
+        const id = request.params.id;
+        const activitiesMock = [
+          {
+            id: 21343252,
+            expenseGroupId: 1,
+            description: "Abastecer",
+            billingDate: new Date(),
+            cost: 200.0,
+          },
+          {
+            id: 23532423,
+            expenseGroupId: 1,
+            description: "Revisão",
+            billingDate: new Date(),
+            cost: 1850.6,
+          },
+          {
+            id: 3,
+            expenseGroupId: 2,
+            description: "Consulta",
+            billingDate: new Date(),
+            cost: 350.0,
+          },
+        ];
+        const activities = activitiesMock.filter(
+          (activity) => activity.expenseGroupId.toString() === id
+        );
+        return { activities };
+      });
     },
   });
 }
